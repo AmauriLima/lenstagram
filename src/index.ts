4f54b0e2bc7e387dea9cfc04ce1735a1084ed074
AmauriLima <amauri.plimaj@gmail.com>
2021-10-18 10:32:43 -0300

    init: initializing rroutes

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import express from 'express';
 import 'reflect-metadata';
 import './database';
 
+import { routes } from './routes';
+
 const app = express();
+
+app.use(routes);
 
 // eslint-disable-next-line no-console
 app.listen(3000, () => console.log('🔥 Server Started at http://localhost:3000'));
