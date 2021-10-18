5b487ac4af8c09be04760a7c4d972c864c90052b
AmauriLima <amauri.plimaj@gmail.com>
2021-10-18 13:31:49 -0300

    feat: verify token middleware

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+interface IDecodedUser {
+  sub: string,
+}
+
+export function verifyToken(request: Request, response: Response, next: NextFunction) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    return response.status(401).json({ error: 'No token provided' });
+  }
+
+  const [scheme, token] = authHeader.split(' ');
+
+  if (!/^Bearer$/i.test(scheme)) {
+    return response.status(401).json({ error: 'Token malformatted' });
+  }
+
+  jwt.verify(token, process.env.SECRET_KEY, (error: Error, decoded: IDecodedUser) => {
+    if (error) {
+      response.status(401).json({ error: 'Invalid token' });
+    }
+    const { sub } = decoded;
+    request.user_id = sub;
+  });
+
+  next();
+}
