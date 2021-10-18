f646c3cd84dfbb596dcd3132d510140c23115a10
AmauriLima <amauri.plimaj@gmail.com>
2021-10-18 11:46:29 -0300

    fix: adding middleware express.json()

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import express from 'express';
 import 'reflect-metadata';
 import './database';
 
-// import { routes } from './routes';
+import { routes } from './routes';
 
 const app = express();
 
-// app.use(routes);
+app.use(express.json());
+app.use(routes);
 
 // eslint-disable-next-line no-console
 app.listen(process.env.API_PORT, () => console.log('🔥 Server Started at http://localhost:3000'));
