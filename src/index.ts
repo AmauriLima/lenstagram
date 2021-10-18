4cfc4e96846d96b71dd0b0561e6770801cd07030
AmauriLima <amauri.plimaj@gmail.com>
2021-10-18 10:56:55 -0300

    "config: adding dotenv"

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import express from 'express';
 import 'reflect-metadata';
 import './database';
 
-import { routes } from './routes';
+// import { routes } from './routes';
 
 const app = express();
 
-app.use(routes);
+// app.use(routes);
 
 // eslint-disable-next-line no-console
-app.listen(3000, () => console.log('🔥 Server Started at http://localhost:3000'));
+app.listen(process.env.API_PORT, () => console.log('🔥 Server Started at http://localhost:3000'));
