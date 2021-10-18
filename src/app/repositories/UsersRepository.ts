ac778113d2b7e3587cbf4a77dc8db5c05a635ee1
AmauriLima <amauri.plimaj@gmail.com>
2021-10-18 11:57:15 -0300

    fix: adding tag EntityRepository to UsersRepository

--- src/app/repositories/UsersRepository.ts
+++ src/app/repositories/UsersRepository.ts
@@ -1,6 +1,7 @@
-import { Repository } from 'typeorm';
+import { EntityRepository, Repository } from 'typeorm';
 import { User } from '../../entities/User';
 
+@EntityRepository(User)
 class UsersRepository extends Repository<User> {}
 
 export default UsersRepository;
