fd7c3521898185b15f3aca19c3d79c458fe2d91e
AmauriLima <amauri.plimaj@gmail.com>
2021-10-25 23:20:03 -0300

    feat: adding posts repository|model|migration

---
+++ src/app/models/Post.ts
@@ -0,0 +1,29 @@
+import {
+  Column, CreateDateColumn, Entity, JoinColumn, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn,
+} from 'typeorm';
+import { User } from './User';
+
+@Entity('posts')
+export class Post {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column()
+  description: string
+
+  @Column()
+  user_id: string
+
+  @JoinColumn({ name: 'user_id' })
+  @OneToMany(() => User, (user) => user.id)
+  userId: User;
+
+  @Column()
+  url_img: string
+
+  @CreateDateColumn()
+  created_at: Date
+
+  @UpdateDateColumn()
+  updated_at: Date
+}
